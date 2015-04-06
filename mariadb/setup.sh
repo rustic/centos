2352635fcc099679bc841c34cc2a48b4ddb9065c
Lee Myers <ichilegend@gmail.com>
2015-04-06 17:10:31 -0400

    Updated Dockerfile for Mariadb

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 # Adapted from: https://gist.github.com/coderua/5592d95970038944d099
+# And https://github.com/CentOS/CentOS-Dockerfiles/blob/master/mariadb/centos7/config_mariadb.sh
+
+__mysql_config() {
+# Hack to get MySQL up and running... I need to look into it more.
+echo "Running the Mariadb init function."
+
+mysql_install_db
+chown -R mysql:mysql /var/lib/mysql
+/usr/bin/mysqld_safe & 
+sleep 10
+}
+
+#Call the function
+__mysql_config
 
 # Setup root password
 CURRENT_MYSQL_PASSWORD=''
-NEW_MYSQL_PASSWORD= `pwgen -cny 12 1`
+NEW_MYSQL_PASSWORD=`pwgen -cn 12 1`
  
 SECURE_MYSQL=$(expect -c "
 
@@ -43,6 +57,6 @@ expect eof
 #
 echo "${SECURE_MYSQL}"
 
-echo "Mariadb username: root Password is: $NEW_MYSQL_PASSWORD"
+echo -e "\033[0;32mMariadb username: \033[0;33mroot \033[0;32mPassword is: \033[0;33m$NEW_MYSQL_PASSWORD\033[0m"
 
 exit 0
